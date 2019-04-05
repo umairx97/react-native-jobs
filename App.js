d4b967d3cc9f5382c5f51afe31fb450c8e768a13
umairx97 <umair.bajwa@hotmail.com>
2019-04-05 20:14:58 +0500

    Added Tab Navigation

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <MainNavigator />;
   }
 }
+
+export const MainNavigator = createBottomTabNavigator({
+  welcome: {
+    screen: WelcomeScreen
+  },
+  auth: {
+    screen: AuthScreen
+  },
+  main: {
+    screen: createBottomTabNavigator({
+      map: {
+        screen: MapScreen
+      },
+      deck: {
+        screen: DeckScreen
+      }
+    })
+  }
+});
+
+export default createAppContainer(MainNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
